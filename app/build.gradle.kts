9d76abe4e1d4be8fdc0eb23938993f794219f99e
pky2119 <pky2119@naver.com>
2023-10-28 15:38:22 +0900

    기초 화면 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "kr.ac.wku.kotlinbasic_20231028"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "kr.ac.wku.kotlinbasic_20231028"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
